0b8e7bcce0b9306be390ecc59c1c2cb80d452fcf
JPhan <jphanacc@gmail.com>
2023-02-02 00:51:07 -0700

    Basic NavBar implementation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react"; //Fragment is component that mounts to nothing to the DOM
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
+import './navigation.styles.scss'
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className='logo-container' to='/'>
+            <CrownLogo />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
