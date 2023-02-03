fd368327561bd2a7d91522a3284631ba8d309af4
JPhan <jphanacc@gmail.com>
2023-02-02 20:03:34 -0700

    added Sign-In component with Google Auth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <h1>
+      Shop component
+    </h1>
+  )
+}
+
 
 const App = () => {
 
@@ -9,6 +21,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation/>}>
         <Route index element={ <Home/> }/>
+        <Route path="shop" element={ <Shop/> }/>
+        <Route path="sign-in" element={ <SignIn/> }/>
       </Route>
     </Routes>
   )
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,61 @@
+//initialize app creates app instance based off of some type of config
+import { initializeApp } from "firebase/app";
+//auth service
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+//import Database stores
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLJFXsVbQVqWIEEHFrOAnMppcwfikEyJw",
+  authDomain: "crown-clothing-db-aa799.firebaseapp.com",
+  projectId: "crown-clothing-db-aa799",
+  storageBucket: "crown-clothing-db-aa799.appspot.com",
+  messagingSenderId: "127143358766",
+  appId: "1:127143358766:web:de2707d60942337eca0abf",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+//For Google Authnetication to work.
+//GoogleAuthProvider is a class we get from Firebase Authentication
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+//create singleton instance that connects to Firebase database
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+    //check if there is document reference
+    //takes 3 arguments: the doc takes db instance, collection name, some identifer
+    const userDocRef = doc(db, "users", userAuth.uid);
+
+    console.log(userDocRef);
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    //if user data does not exist
+    //if user data exists
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            //then create a userDoc reference using the snapshot
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (err) {
+            console.log('error creating the user', err.message)
+        }
+    }
+
+    return userDocRef;
+};
