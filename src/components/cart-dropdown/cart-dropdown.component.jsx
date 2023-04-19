a5cf938a28ca5fbe9b61ca7e8a6796b50c73ed21
JPhan <jphanacc@gmail.com>
2023-04-19 17:07:17 -0600

    updated CartDropdown, DirectoryItem and CartIcon to styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,7 @@ import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss';
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
 
@@ -17,15 +17,20 @@ const CartDropdown = () => {
         navigate('/checkout');
     }
     return (
-        <div className='cart-dropdown-container'>
-            <div className="cart-items"/> 
-                {cartItems.map((cartItem) => {
-                    return <CartItem key={cartItem.id} cartItem={cartItem}/>
-                })}
-                
+        <CartDropdownContainer>
+            <CartItems>
+                {
+                    cartItems.length
+                        ? (cartItems.map((cartItem) => {
+                            return <CartItem key={cartItem.id} cartItem={cartItem} />
+                        }))
+                        : (<EmptyMessage>Your cart is empty</EmptyMessage>)
+                }
+            </CartItems>
+
             <Button onClick={goToCheckoutHandler}>Go To Checkout</Button>
-            
-        </div>
+
+        </CartDropdownContainer>
     )
 }
 
